f754ca03c4af3d6e90b76e18490293408b9259a7
Shalini Singh <131110991+Shalini06singh@users.noreply.github.com>
2024-12-21 14:31:26 +0000

    set homepage

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/design-desk.webp";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Movie and Show Tracker App🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A user-friendly React application for tracking and exploring movies and TV shows.",
+    url: "https://movie-show-tracker.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development for TODOLISTAPP",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      " The app allows users to create, read, update, and delete their tasks efficiently.",
+    url: "https://todolistapp-self.vercel.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Theme Toggler",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created for Theme Switching using the Context API Includes my experience and design abilities.",
+    url: "https://theme-toggler-plum.vercel.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "React-project",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      " Interactive to explain when to use React.js as a React application along react-router-dom.",
+    url: " https://react-project-nine-peach.vercel.app/",
   },
 ];
 
